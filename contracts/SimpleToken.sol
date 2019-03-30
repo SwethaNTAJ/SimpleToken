4d45babc0ae38559083ae1314f4488388daa92f9
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-03-30 16:07:14 +0530

    Add mint method

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -4,8 +4,8 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract SimpleToken is ERC20, ERC20Detailed {
-	constructor(string memory _name, string memory _symbol, uint8 _decimals) 
+	constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) 
 		ERC20Detailed(_name, _symbol, _decimals) public {
-		
+			_mint(msg.sender, _initialSupply.mul(10**uint256(_decimals)));
 	}
 }
--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -4,7 +4,8 @@ module.exports = function(deployer) {
   const _name = 'Simple Token'
   const _symbol = 'STK';
   const _decimals = 8;
+  const _amount = 6543210;
   
-  deployer.deploy(SimpleToken, _name, _symbol, _decimals);
+  deployer.deploy(SimpleToken, _name, _symbol, _decimals, _amount);
 };
 
